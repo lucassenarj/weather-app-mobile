88dc5e3e3dc244ada8633099fa9ce799cee3fce1
Lucas Sena <lucassena.rj@gmail.com>
2022-01-24 12:27:06 +0000

    fix(routes): add custom style to routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,29 +1,102 @@
 import React from "react";
-import { SafeAreaView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesome5 } from "@expo/vector-icons";
+import colors from "../styles/colors";
+import Forecast from "../pages/Forecast";
+import Location from "../pages/Location";
+import Report from "../pages/Report";
+import Settings from "../pages/Settings";
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 function Routes(){
   return (
     <NavigationContainer>
-      <Navigator>
+      <Navigator
+        screenOptions={{
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: colors.white,
+          tabBarInactiveTintColor: colors.gray,
+          tabBarStyle: {
+            height: 100,
+            borderTopWidth: 0,
+            alignItems: "center",
+            justifyContent: "center",
+            position: "absolute",
+            backgroundColor: colors.background,
+          },
+          tabBarIconStyle: {
+            width: 20,
+            height: 20,
+            marginTop: 10,
+          },
+          headerTransparent: true,
+          headerTitleStyle: {
+            color: colors.white
+          }
+        }}
+      >
         <Screen
           name="Forecast"
-          component={SafeAreaView}
+          component={Forecast}
+          options={{
+            tabBarIcon: ({ size, focused }) => {
+              return (
+                <FontAwesome5
+                  name="home"
+                  size={size}
+                  color={ focused ? colors.white : colors.gray }
+                />
+              );
+            }
+          }}
         />
         <Screen
           name="Location"
-          component={SafeAreaView}
+          component={Location}
+          options={{
+            tabBarIcon: ({ size, focused }) => {
+              return (
+                <FontAwesome5
+                  name="search"
+                  size={size}
+                  color={ focused ? colors.white : colors.gray }
+                />
+              );
+            }
+          }}
         />
         <Screen
           name="Report"
-          component={SafeAreaView}
+          component={Report}
+          options={{
+            tabBarIcon: ({ size, focused }) => {
+              return (
+                <FontAwesome5
+                  name="bullhorn"
+                  size={size}
+                  color={ focused ? colors.white : colors.gray }
+                />
+              );
+            }
+          }}
         />
         <Screen
           name="Settings"
-          component={SafeAreaView}
+          component={Settings}
+          options={{
+            tabBarIcon: ({ size, focused }) => {
+              return (
+                <FontAwesome5
+                  name="cog"
+                  size={size}
+                  color={ focused ? colors.white : colors.gray }
+                />
+              );
+            }
+          }}
         />
       </Navigator>
     </NavigationContainer>
