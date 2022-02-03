e9402b9ef083b4a7ea7103638cd4fdb8c35f66b7
Lucas Sena <lucassena.rj@gmail.com>
2022-02-03 10:34:41 +0000

    fix(types): fix getFullDay

--- src/utils/getFullDay.ts
+++ src/utils/getFullDay.ts
@@ -1,25 +1,25 @@
 export default function getFullDay(day: number): string {
   switch(day) {
     case 0: {
-      return "Monday";
+      return "Sunday";
     }
     case 1: {
-      return "Tuesday";
+      return "Monday";
     }
     case 2: {
-      return "Wednesday";
+      return "Tuesday";
     }
     case 3: {
-      return "Thursday";
+      return "Wednesday";
     }
     case 4: {
-      return "Friday";
+      return "Thursday";
     }
     case 5: {
-      return "Saturday";
+      return "Friday";
     }
     case 6: {
-      return "Sunday";
+      return "Saturday";
     }
   }
 }
