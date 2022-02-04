f298a6adcea829e70a8a7d406ecbff62187b037a
Lucas Sena <lucassena.rj@gmail.com>
2022-02-04 16:32:31 +0000

    feat(search): add get latitude and longitude functionality

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,14 +1,31 @@
 import { useState } from "react";
 import { FontAwesome5 } from "@expo/vector-icons";
+import * as Location from 'expo-location';
 import colors from "../../styles/colors";
 import {
   Section,
   Input,
   Button,
 } from "./styles";
+import api from "../../services/api";
 
 function Search() {
   const [value, setValue] = useState(null);
+  const [locations, setLocations] = useState([]);
+
+  async function handleSelectLocation() {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      console.log("Permission denied");
+      return;
+    }
+    const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
+    const { status: apiStatus, response } = await api.searchWeatherByLatLong(latitude, longitude);
+    if (apiStatus === 200) {
+      setLocations(response);
+    }
+  };
+
   return (
     <Section>
       <Input
@@ -18,7 +35,7 @@ function Search() {
         value={value}
         onChangeText={setValue}
       />
-      <Button>
+      <Button onPress={handleSelectLocation}>
         <FontAwesome5
           size={30}
           name="map-marker-alt"
